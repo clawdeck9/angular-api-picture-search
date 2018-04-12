e77370aac9ddc873a84f1a1a221fea6811bd5f1a
ClawDeck <clawdeck9@gmail.com>
2018-04-12 16:18:19 +0200

    up to video 3

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  
+  info = {
+    nom: "Claw",
+    email: "clawdeck.gmail.com",
+    tel: "12345678"
+  }
+  
+  comments = [
+    {date:new Date(), message:"AA"},
+    {date:new Date(), message:"BB"},
+    {date:new Date(), message:"CC"}     
+  ];
 
   constructor() { }
 
