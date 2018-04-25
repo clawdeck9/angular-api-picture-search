1e87847ffe940b7eb07e8b9906c9709d09e159e2
ClawDeck <clawdeck9@gmail.com>
2018-04-25 17:14:20 +0200

    added butons in the main component that use Routes mechanism, works fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import {AboutService} from "../services/about.service";
 import {FormsModule} from "@angular/forms";
+import { ContactsComponent } from './contacts/contacts.component';
+
+const routes:Routes=[
+  {path:'about',component:AboutComponent},
+  {path:'contacts',component:ContactsComponent},
+  {path:'',redirectTo:'about',pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [AboutService],
   bootstrap: [AppComponent]
