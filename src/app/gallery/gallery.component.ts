ba89114af5132799a52f32152a76d4205a140551
ClawDeck <clawdeck9@gmail.com>
2018-05-04 19:08:54 +0200

    added searchItem class

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -8,6 +8,12 @@ import {Observable} from 'rxjs';
   styleUrls: ['./gallery.component.css']
 })
 
+class SearchItem {
+  constructor(public tags: string,
+              public previewURL: string) {
+  }
+}
+
 
 export class GalleryComponent implements OnInit {
   pages:any;
@@ -22,9 +28,8 @@ export class GalleryComponent implements OnInit {
   onSearch(fvalue){
     console.log(fvalue);
     let obs = this.http.get("https://pixabay.com/api/?key=8821333-78c43370cf39bfee2eb2a9273&q="+fvalue.keyword+"&per_page=5&page=1")
-              .map(resp => {return resp.json().hits.map(hit => {return hit.downloads;});});
+              .map(resp => {return resp.json().hits.map(hit => {return new SearchItem(hit.tags, hit.previewURL);});});
     obs.subscribe(data => this.pages = data);
     console.log(this.pages);
-
   }
 }
