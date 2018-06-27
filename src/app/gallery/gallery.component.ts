2eff6b37379e8456b810d704d504c2d8a9df5e85
ClawDeck9 <clawdeck9@gmail.com>
2018-06-27 15:02:14 +0200

    a series of 5 pictures is now displayed after being retrieved from the web API

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {SearchService} from "../../services/search.service";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
@@ -11,6 +12,7 @@ import {SearchService} from "../../services/search.service";
 export class GalleryComponent implements OnInit {
   pages:any={hits:[]};
   picture:any;
+  obs: Observable<any>;
 
   constructor(private searchService:SearchService) {
   }
@@ -20,7 +22,14 @@ export class GalleryComponent implements OnInit {
   }
 
   onSearch(fvalue){
-    this.pages = this.searchService.search(fvalue);
+    this.obs = this.searchService.search(fvalue);
+    this.obs.subscribe((data) => {
+                                this.pages = data;
+                                console.log("in subscribe" + data);
+                                console.log("in subscribe" + this.pages);}
+                              );
+  
+
     console.log("SearchComponent : " + this.pages);
   }
 }
--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -16,14 +16,8 @@ export class SearchService {
 
   search(fvalue){
     console.log("fvalue = " + fvalue);
-    let obs = this.http.get("https://pixabay.com/api/?key=8821333-78c43370cf39bfee2eb2a9273&q="+fvalue.keyword+"&per_page=5&page=1")
+    this.obs = this.http.get("https://pixabay.com/api/?key=8821333-78c43370cf39bfee2eb2a9273&q="+fvalue.keyword+"&per_page=5&page=1")
               .map(resp => {return resp.json().hits.map(hit => {return  hit.previewURL;});});
-    obs.subscribe((value) => {
-                                this.pages = value;
-                                console.log("in subscribe" + value);
-                                console.log("in subscribe" + this.pages);
-                              });
-    console.log("this.pages = " + this.pages);
-    return this.pages;
+    return this.obs;
   }
 }
