c1df3141ba0c1702335d34ec5aaafa84f5c38dc4
ClawDeck9 <clawdeck9@gmail.com>
2018-08-22 09:32:59 +0200

    temp -  pills must be added in GalleryComponent

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SearchService} from "../../services/search.service";
 import { Observable } from 'rxjs';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-gallery',
@@ -11,7 +12,10 @@ import { Observable } from 'rxjs';
 
 export class GalleryComponent implements OnInit {
   pages:any={hits:[]};
-  picture:any;
+  keyword:any="cat";
+  pageNumber:any=1;
+  totalPages:any;
+  pillsIndex:any[];
   obs: Observable<any>;
 
   constructor(private searchService:SearchService) {
@@ -19,17 +23,35 @@ export class GalleryComponent implements OnInit {
 
 
   ngOnInit() {
+    console.log("ngOnInit()");
   }
-
-  onSearch(fvalue){
-    this.obs = this.searchService.search(fvalue);
+  onSearch(f: NgForm){
+    this.obs = this.searchService.search(f.value.keyword,f.value.pageNumber,f.value.pageLength);
+    this.keyword = f.value.keyword;
     this.obs.subscribe((data) => {
-                                this.pages = data;
-                                console.log("in subscribe" + data);
-                                console.log("in subscribe" + this.pages);}
-                              );
-  
+                                  this.pages = data.hits.map(hit => {return  hit;});
+                                  this.totalPages = (data.totalHits/f.value.pageLength).toFixed(0);
+                                  if (this.totalPages<1) this.totalPages = 1;
+                                  this.pillsIndex = new Array(this.totalPages);
+                                  console.log(f.value);  // { first: '', last: '' }
+                                  console.log(f.valid);  // false
 
-    console.log("SearchComponent : " + this.pages);
+
+                                  console.log("totalHits = " + data.totalHits);
+                                  console.log("page length = " + f.value.pageLength);
+                                  console.log("totalPages = " + this.totalPages);
+                                  console.log("pillsIndex = " + this.pillsIndex);
+                                  // console.log("(indexOf(data)) % pageLength = " + (data.indexOf()) % pageLength);
+                                  //  pillsIndex = hits.map(data => if((indexOf(data)) % pageLength)=== 0 )return (indexOf(data)) % pageLength ;)
+                                  //  cette fonction produit un index pour chaque page mais rest à verifier
+                                  //  voir si fonction length existe pour un tableau
+                                  //  totalPages est-il un entier? si oui peut etre egal à 0
+                                  //  stasher cette branche et revenir au dernier add pour faire des tests dur les variable liées template/ts
+                                }
+                              );
+
+
+
+    console.log("Search.gallery.Component");
   }
 }
--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -14,10 +14,10 @@ export class SearchService {
 
   constructor(private http:Http) { }
 
-  search(fvalue){
+  search(fvalue,pageNumber,pageLength){
     console.log("fvalue = " + fvalue);
-    this.obs = this.http.get("https://pixabay.com/api/?key=8821333-78c43370cf39bfee2eb2a9273&q="+fvalue.keyword+"&per_page=5&page=1")
-              .map(resp => {return resp.json().hits.map(hit => {return  hit.previewURL;});});
+    this.obs = this.http.get("https://pixabay.com/api/?key=8821333-78c43370cf39bfee2eb2a9273&q="+fvalue.keyword+"&per_page="+pageLength+"&page="+pageNumber)
+              .map(resp => {return resp.json();});
     return this.obs;
   }
 }
